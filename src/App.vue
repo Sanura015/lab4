4aaabfc4a3681ae9692a043635334ed716863212
DESKTOP-25CL02E\Banana444 <614234015@parichat.skru.ac.th>
2021-01-24 23:36:30 +0700

    HW1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="Student-Front">
+      <center>
+        <h1>Information Me & Friends</h1>
+      </center>
+  </div>
+  <StudentProperty v-bind:StudentIndex="StudentProperty" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StudentProperty from "./components/StudentProperty.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StudentProperty,
+  },
+  data(){
+    return{
+      StudentProperty:[
+        {
+          studentName: "KAH",
+          studentId: "61234010",
+          FirstName: "Nur-ateekah",
+          LastName: "Salaeh",
+          weight: 70,
+          height: 152,
+          picture: 
+            "https://raw.githubusercontent.com/Sanura015/IMG/main/Kah.jpg",
+        },
+        {
+          studentName: "NURA",
+          studentId: "61234015",
+          FirstName: "Sanura",
+          LastName: "Chengoh",
+          weight: 65,
+          height: 158,
+          picture: 
+            "https://raw.githubusercontent.com/Sanura015/IMG/main/Nura.jpg",
+        },
+        {
+          studentName: "GUN",
+          studentId: "61234012",
+          FirstName: "Naspat",
+          LastName: "Stiramitakon",
+          weight: 63,
+          height: 162,
+          picture: 
+            "https://raw.githubusercontent.com/Sanura015/IMG/main/Gun.jpg",
+        },
+        {
+          studentName: "YUI",
+          studentId: "61234007",
+          FirstName: "Suphakin",
+          LastName: "Jaroenmak",
+          weight: 55,
+          height: 163,
+          picture: 
+            "https://raw.githubusercontent.com/Sanura015/IMG/main/Yui.png",
+        },
+        {
+          studentName: "Pari",
+          studentId: "61234013",
+          FirstName: "Priyaphat",
+          LastName: "Jaroerit",
+          weight: 65,
+          height: 160,
+          picture: 
+            "https://raw.githubusercontent.com/Sanura015/IMG/main/Pari.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Sedgwick Ave Display", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+  font-size: 20px;
+  padding: 20px 0px 0px 100px;
+  background: rgb(158, 202, 228) ;
+}
+
+.Student-Front{
+  font: size 50px;
+  
 }
 </style>
---
+++ src/components/StudentDetail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="Student-Detali">
+        <h1>{{ Student.title }}</h1>
+        <br />
+        <div style="padding: 0px 0px 0px 2px; font-size: 40px">
+            <b>NAME</b> {{ Student.studentName }} <br />
+        </div>
+        <br />
+        <div>
+            <img
+            style="float: middle; padding: 20px"
+            width="250px"
+            v-bind:src="Student.picture"
+            />
+        </div>
+        <div style="middle: 0px 0px 10px 400px">
+        <br /><b>StudentId:614234010</b>{{ Student.StudentId }} <br />
+        <br /><b>FirstName:</b> {{ Student.FirstName }} <br />
+        <br /><b>LastName:</b> {{ Student.LastName }} <br />
+        <br /><b>Weight:</b> {{ Student.weight }} <br />
+        <br /><b>Height:</b> {{ Student.height }} <br />
+        <br />
+        <br />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StudentDetail",
+    components:{},
+    props: ["Student"],
+};
+</script>
+
+<style>
+.Student-Detail {
+    background: rgb(fff,fff,fff);
+    padding: 20px 30px 0px 400px;
+    border-bottom-color: aqua 1px;
+}
+</style>
